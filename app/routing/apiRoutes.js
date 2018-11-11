d07e4fff4882a7b97ba8049cf3287f5eeaf65207
Ian Richard <ianrichard03@gmail.com>
2018-11-11 17:34:13 -0600

    added nerdTest function to apiRoutes.js.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,6 +13,39 @@ module.exports = function(app) {
     let newNerd = request.body;  
     newNerd.routeName = newNerd.name.replace(/\s+/g,"").toLowerCase();
     nerds.push(newNerd);   
-    response.json(newNerd);
+    
+    let matchNerd = nerdTest(newNerd);
+    
+    response.json(matchNerd.name);
+
   });
 };
+
+function nerdTest(object){
+  let variance =0;
+  let matchArray = [];
+  //loop through each nerd record object
+  nerds.forEach(function(element) {
+      //console.log(element);
+      //loop through each nerd record answer
+      for(let i = 0; i <= 9; i++){
+              variance += (element[i] > object.answers[i]) ? (element[i] - object.answers[i]) : (object.answers[i] - element[i]);
+          
+      }
+      //console.log(variance);
+      matchArray.push(variance);
+      variance = 0;
+  });
+  console.log(matchArray);
+  let lowestNumber = 40;  //assuming max variance of 40, if an element is lower than this, it becomes low number.
+  let pick = 0;
+  for(let j = 0; j <= matchArray.length; j++){
+      if(matchArray[j] < lowestNumber){
+          lowestNumber = matchArray[j];
+          pick = j -1;
+      }
+  }
+  console.log("Lowest number is: " + lowestNumber + "\nMatch is: " + nerds.answer[pick]);
+  return nerds.answers[pick];
+}
+
