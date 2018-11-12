daa03bcd55ff196a0c2f872443a7b9d3494ad3b2
Ian Richard <ianrichard03@gmail.com>
2018-11-11 21:03:40 -0600

    fixed form submit.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,16 @@
 //Nerds Array stores my nerd objects.
 const nerds = [
-/** {
+    {
           name: "Ian Richard",
           photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/268714_10150242813262509_4970049_n.jpg?_nc_cat=100&_nc_ht=scontent-lax3-1.xx&oh=b3743eb46c82b8988d59da97f354731b&oe=5C7A0E90",
           answers: [5,1,3,1,1,1,3,5,5,1],
           matches: [],
           routeName: "ianrichard"
     },
- */
+ 
     {
-        name: "Match Control",
-        photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/268714_10150242813262509_4970049_n.jpg?_nc_cat=100&_nc_ht=scontent-lax3-1.xx&oh=b3743eb46c82b8988d59da97f354731b&oe=5C7A0E90",
+        name: "Johnny 5",
+        photo: "https://i.pinimg.com/originals/c7/98/6c/c7986cb4b3017d976cbf17efcb184a36.jpg",
         answers: [5,5,5,5,5,5,5,5,5,5],
         matches: [],
         routeName: "matchcontrol"
@@ -18,12 +18,11 @@ const nerds = [
 
     {
         name: "Bruce Campbell",
-        photo: "https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/268714_10150242813262509_4970049_n.jpg?_nc_cat=100&_nc_ht=scontent-lax3-1.xx&oh=b3743eb46c82b8988d59da97f354731b&oe=5C7A0E90",
+        photo: "https://nerdist.com/wp-content/uploads/2015/07/Bruce-Campbell.jpg",
         answers: [1,1,1,1,1,1,1,1,1,1],
         matches: [],
         routeName: "brucecampbell"
     },
-
 
 ];
 
